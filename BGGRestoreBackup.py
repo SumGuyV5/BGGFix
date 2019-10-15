c9b81a1ae5eb6371eae4e0a2bfa7036ff8b4869d
Richard Allen <richard2891@gmail.com>
2019-10-14 21:19:29 -0400

    if we get a file error return with out trying to read it.

--- BGGRestoreBackup.py
+++ BGGRestoreBackup.py
@@ -1,26 +1,24 @@
-#!/usr/bin/python
-import configparser
 import os
 import time
-
 import lxml.html
 import requests
+import configparser
+import BGGModule.Functions
 
 from BGGModule.DownloadXML import DownloadXML
 from BGGModule.ReadXML import ReadXML
 
-import BGGModule.Functions
 
-
-class BGGFix:
+class BGGRestoreBackup:
     def __init__(self):
         config = configparser.RawConfigParser()
         config.read("creds")
 
-        self.change_from = '1'  # the name to be search for and found.
-        self.change_to = '0'  # what name should be renamed to.
-        self.change_attrib = 'nowinstats'
-        self.player = False
+        self.backup = []
+        self.current = []
+        self.restore = []
+        self.backup_dir = os.path.join(os.getcwd(), "Backup/plays")
+        self.backup_count = 12
         self.bgg_user = config.get('BGG', 'user')  # board game geek username that all the plays are recorded under.
         self.bgg_password = config.get('BGG', 'pass')  # board game geek password.
         self.pagesize = 100  # how many plays per xml file. 100 is the max.
@@ -35,10 +33,10 @@ class BGGFix:
 
         :return: None
         """
-        # self.retrieve_xml()  # downloads all the xml files with the info we need
+        #self.retrieve_xml()  # downloads all the xml files with the info we need
         self.read_xml()  # reads the xml files and finds all the id's for the recorded plays that need to be fix.
         self.login_bgg()  # login
-        self.play_edit_all()
+        self.play_restore_all()
 
     def login_bgg(self):
         """
@@ -76,10 +74,9 @@ class BGGFix:
             except KeyError:
                 print(f'Error {x.attrib["name"]}')
 
-        return found, value
+        return found, str(value)
 
-
-    def play_key_found(self, play_num):
+    def play_found(self, play_num):
         """
         We check the play_num to see if the name we are looking to change is in the play record.
 
@@ -94,6 +91,7 @@ class BGGFix:
             quit()
         else:
             print(f'{self.bgg_user} is logged in.')
+            found = True
 
         page_html = lxml.html.fromstring(response.text)
 
@@ -102,38 +100,48 @@ class BGGFix:
 
         for x in hidden_inputs:
             try:
-                form[x.attrib["name"]] = x.attrib["value"]
                 found_check, value = self.checkbox_check(x)
                 if found_check:
                     form[x.attrib["name"]] = value
-                if x.attrib["name"] == self.change_attrib and x.attrib["value"] == self.change_from:
-                    found = True
+                else:
+                    form[x.attrib["name"]] = x.attrib["value"]
             except KeyError:
+                form[x.attrib["name"]] = ''
                 print(f'Error {x.attrib["name"]}')
 
         print(form)
 
         return found, form
 
-    def play_edit(self, play_num):
+    def play_restore(self, play):
         """
         We go to the play that is to be change and read the data off of the page.
         We then make the need change and pass the changes to geekplay.php to save.
 
-        :param play_num: The play number to edit.
+        :param play: The play number to edit.
         :return: None
         """
-        found, form = self.play_key_found(play_num)
+        lst = ['location', 'quantity', 'length', 'incomplete', 'nowinstats']
+        found, form = self.play_found(play.id)
         if found:
-            print(f'{self.change_from} was found.')
+            print(f'{play.id} was found.')
         else:
-            print(f'{self.change_from} was Not found.')
+            print(f'{play.id} was Not found.')
             return
 
         for key, value in form.items():
-            if key == self.change_attrib and value == self.change_from:
-                form[key] = self.change_to
-                print(f'{key} = {value} to {key} = {self.change_to}')
+            if key == 'dateinput':
+                if value != play.date:
+                    form[key] = play.date_str()
+            else:
+                if key in lst:
+                    val = play.__getattribute__(key)
+                    if type(val) is bool:
+                        val = int(val)
+                    form[key] = str(val)
+
+
+        print(form)
 
         if self.dryRun:
             print('This is a dry run. We will stop here.')
@@ -142,23 +150,23 @@ class BGGFix:
         time.sleep(2)  # lets not hit the server to hard
         self.session.post('https://www.boardgamegeek.com/geekplay.php', data=form)
 
-        found, form = self.play_key_found(play_num)
+        found, form = self.play_found(play.id)
         if found:
             print(f'{self.change_from} was found.')
         else:
             print(f'{self.change_from} was Not found.')
             return
 
-    def play_edit_all(self):
+    def play_restore_all(self):
         """
         Prints out the total number of play records to be edit and the current play being edited.
         :return:
         """
-        for idx, play_num in enumerate(self.play_nums):
+        for idx, play in enumerate(self.restore):
             print('====================================')
-            print(f'Play {idx + 1} of {len(self.play_nums)}')
+            print(f'Play {idx + 1} of {len(self.restore)}')
             print('====================================')
-            self.play_edit(play_num)
+            self.play_restore(play)
 
     def retrieve_xml(self):
         """
@@ -178,19 +186,17 @@ class BGGFix:
         """
         read_xml = ReadXML()
         read_xml.read_xml_all(os.path.join(os.getcwd(), "plays"), self.count_to)
+        self.current = read_xml.plays
 
-        for play in read_xml.plays:
-            if self.player:
-                for player in play.players:
-                    if player.__getattribute__(self.change_attrib) == self.change_from:
-                        self.play_nums.append(play.id)
-                        break
-            else:
-                if play.__getattribute__(self.change_attrib) == self.change_from:
-                    self.play_nums.append(play.id)
-        print(self.play_nums)
+        read_xml.read_xml_all(self.backup_dir, self.backup_count)
+        self.backup = read_xml.plays
 
+        for cur in self.current:
+            for back in self.backup:
+                if cur.id == back.id:
+                    if cur != back:
+                        self.restore.append(back)
 
 if __name__ == "__main__":
-    bgg_fix = BGGFix()
-    bgg_fix.main()
+    main = BGGRestoreBackup()
+    main.main()
